0268689053aca950b86e501c90b5b7fc4ab6e227
Lucas <icreateonnt@gmail.com>
2026-09-06 21:46:04 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int num1;
+    int num2;
+
+    int choice; 
+    
+    std::cout << "Enter first number (make sure the order is correct!): " << std::endl;
+    std::cin >> num1;
+
+    std::cout << "Enter second number (make sure the order is correct!): " << std::endl;
+    std::cin >> num2;
+
+
+}
