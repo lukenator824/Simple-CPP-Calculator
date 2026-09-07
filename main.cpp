e9b949fe38fd652e42f47b54987ab93f925b75a2
Lucas <icreateonnt@gmail.com>
2026-09-07 11:56:55 -0400

    Replaced the if statement and added 2 more operations.

--- main.cpp
+++ main.cpp
@@ -12,18 +12,30 @@ int main() {
     std::cout << "Enter second number (make sure the order is correct!): " << std::endl;
     std::cin >> num2;
 
-    std::cout << "Enter a choice (1 for addition, 2 for subtraction): " << std::endl;
+    std::cout << "Enter a choice (1 for addition, 2 for subtraction, 3 for multiplication, 4 for division): " << std::endl;
     std::cin >> choice;
 
 
-    if (choice == 1) {
-        std::cout << "The sum is: " << num1 + num2 << std::endl;
-    } else if (choice == 2) {
-        std::cout << "The difference is: " << num1 - num2 << std::endl;
-    } else {
-        std::cout << "Invalid choice!" << std::endl;
-        return 0;
-    } 
+    switch (choice) {
+        case 1:
+            std::cout << "The sum is: " << num1 + num2 << std::endl;
+            break;
+        case 2:
+            std::cout << "The difference is: " << num1 - num2 << std::endl;
+            break;
+        case 3:
+            std::cout << "The product is: " << num1 * num2 << std::endl;
+            break;
+        case 4:
+            if (num2 != 0) {
+                std::cout << "The quotient is: " << num1 / num2 << std::endl;
+            } else {
+                std::cout << "Error: Division by zero is not allowed!" << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Invalid choice!" << std::endl;
+    }
 
     std::cout << "Hope you enjoyed my calculator!" << std::endl;
     return 0;
